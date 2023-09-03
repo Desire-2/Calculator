7518c54eb9016934ff361ee09fabc18551f5e7f1
Desire-2 <bikorimanadesire5@gmail.com>
2023-09-03 16:42:31 +0200

    Update Calculator

--- calculator.js
+++ calculator.js
@@ -1,123 +1,139 @@
+// script.js
+
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll('.btn[data-number]');
+const operatorButtons = document.querySelectorAll('.btn[data-operator]');
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const pointButton = document.getElementById('pointBtn');
+const lastOperationScreen = document.getElementById('lastOperationScreen');
+const currentOperationScreen = document.getElementById('currentOperationScreen');
+
+// Add event listeners
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => appendNumber(button.textContent));
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => setOperation(button.textContent));
+});
+
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+pointButton.addEventListener('click', appendPoint);
+
+// Keyboard input event listener
+window.addEventListener('keydown', handleKeyboardInput);
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+    resetScreen();
+  currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = '';
+  shouldResetScreen = false;
+}
+
+function clear() {
+  currentOperationScreen.textContent = '0';
+  lastOperationScreen.textContent = '';
+  firstOperand = '';
+  secondOperand = '';
+  currentOperation = null;
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen();
+  if (currentOperationScreen.textContent === '') currentOperationScreen.textContent = '0';
+  if (currentOperationScreen.textContent.includes('.')) return;
+  currentOperationScreen.textContent += '.';
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === '.') appendPoint();
+  if (e.key === '=' || e.key === 'Enter') evaluate();
+  if (e.key === 'Backspace') deleteNumber();
+  if (e.key === 'Escape') clear();
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+    setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === '/') return '÷';
+  if (keyboardOperator === '*') return '×';
+  if (keyboardOperator === '-') return '−';
+  if (keyboardOperator === '+') return '+';
+}
+
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return "Cannot divide by 0";
-    }
+  return a / b;
 }
 
-function percentage(a) {
-    return a / 100;
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '−':
+      return subtract(a, b);
+    case '×':
+      return multiply(a, b);
+    case '÷':
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
 }
-
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        case "%":
-            return percentage(num1);
-        default:
-            return "Invalid operator";
-    }
-}
-
-let currentInput = "";
-let firstNumber = null;
-let operator = null;
-
-function updateDisplay(value) {
-    const display = document.getElementById("display");
-    display.value = value;
-}
-
-document.querySelectorAll(".digit, .operator").forEach(button => {
-    button.addEventListener("click", () => {
-        const buttonText = button.textContent;
-        currentInput += buttonText;
-        updateDisplay(currentInput);
-    });
-});
-
-document.querySelector(".decimal").addEventListener("click", () => {
-    if (!currentInput.includes(".")) {
-        currentInput += ".";
-        updateDisplay(currentInput);
-    }
-});
-
-document.querySelector(".backspace").addEventListener("click", () => {
-    currentInput = currentInput.slice(0, -1);
-    updateDisplay(currentInput);
-});
-
-document.querySelector(".equals").addEventListener("click", () => {
-    performCalculation();
-    updateDisplay();
-});
-
-document.querySelector(".enter").addEventListener("click", () => {
-    performCalculation();
-    updateDisplay();
-
-});
-
-function performCalculation() {
-    if (firstNumber !== null && operator !== null) {
-        const secondNumber = currentInput === "" ? 0 : parseFloat(currentInput);
-        const result = operate(operator, firstNumber, secondNumber);
-
-        firstNumber = null;
-        operator = null;
-        currentInput = result.toString();
-        updateDisplay(result);
-    }
-}
-
-document.querySelector(".divide").addEventListener("click", () => {
-    if (currentInput === "0") {
-        updateDisplay("Cannot divide by 0");
-        currentInput = "";
-    } else {
-        operator = "/";
-        updateDisplay(currentInput + operator);
-        firstNumber = parseFloat(currentInput);
-        currentInput = "";
-    }
-});
-
-document.addEventListener("keydown", event => {
-    const key = event.key;
-
-    if ((key >= "0" && key <= "9") || key === "+" || key === "-" || key === "*" || key === "/" || key === "%") {
-        currentInput += key;
-        updateDisplay(currentInput);
-    }
-
-    if (key === "Enter") {
-        event.preventDefault();  // Prevent default behavior of Enter key
-        performCalculation();    // Perform the calculation
-    }
-
-    if (key === "Backspace") {
-        currentInput = currentInput.slice(0, -1);
-        updateDisplay(currentInput);
-    }
-});
